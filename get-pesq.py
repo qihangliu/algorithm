b52bd4de184a25b29fee139de94e54f861adf9e1
Emancipator <57081465+qihangliu@users.noreply.github.com>
2023-05-16 08:42:58 +0800

    Add files via upload

--- get-pesq.py
+++ get-pesq.py
@@ -1,32 +1,40 @@
-from pydub import AudioSegment
-import pesq
+import os
+import sys
 
-# 设置参考音频文件路径
-ref_path = 'reference.wav'
+clear_filepath = sys.argv[1]
+noise_filepath = sys.argv[2]
+simple_rate = sys.argv[3]
 
-# 定义得分字典
-scores = {}
 
-# 循环遍历所有的测试音频文件
-for degraded_path in ['sample1.wav', 'sample2.wav', 'sample3.wav']:
 
-    # 加载音频
-    ref_audio = AudioSegment.from_wav(ref_path)
-    degraded_audio = AudioSegment.from_wav(degraded_path)
+def signal_pesq(clear_filepath, noise_filepath, simple_rate):
+    '''
+    P.862 提供的原始 PESQ 得分为 0.5 到 4.5 分。为了获得可以与 MOS 分值相比较的得分，需要将原始得分映射为 MOS-LQO，值越大表示效果越好。
+    :param clear_filepath:
+    :param noise_filepath:
+    :param simple_rate:
+    :return:
+    '''
 
-    # 转换采样率为8000Hz
-    ref_audio = ref_audio.set_frame_rate(8000)
-    degraded_audio = degraded_audio.set_frame_rate(8000)
+    cmd = '.\pesq.exe +{} {} {}'.format(simple_rate,clear_filepath,noise_filepath)
+    os.system(cmd)
+    pesq_information = os.popen(cmd).readlines()[-1]
 
-    # 将音频数据转换为numpy数组
-    ref_data = ref_audio.get_array_of_samples()
-    degraded_data = degraded_audio.get_array_of_samples()
+    # print(re.split('=',pesq_information.strip()))
+    print(pesq_information)
 
-    # 计算PESQ分数
-    score = pesq.pesq(8000, ref_data, degraded_data, 'wb')
+    raw_mos,mos_lqo = re.split('\t',re.split('=',pesq_information.strip())[-1])
+    raw_mos = float(raw_mos)
+    mos_lqo = float(mos_lqo)
 
-    # 将分数添加到得分字典中
-    scores[degraded_path] = score
+    # os.popen('CMD').readlines()[0]
+    # information = os.popen(cmd).read()
+    print(raw_mos,mos_lqo)
 
-# 输出得分结果
-print(scores)
+    return mos_lqo
+
+if __name__ == '__main__':
+
+
+    signal_pesq(clear_filepath,noise_filepath,simple_rate)
+
